706feaf2f8a94f9a54246717f1af70a2b6dc2b42
猫耳 <1033578021@qq.com>
2024-09-20 14:56:58 +0800

    1.删除注释代码

--- python_server.py
+++ python_server.py
@@ -25,35 +25,6 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
         #确保返回的路径在指定的目录内
         path = os.path.normpath(os.path.join(DIRECTORY, path.lstrip('/')))
         return path
-
-    '''def do_GET(self):
-        try:
-            # 对URL进行解码
-            path = unquote(self.path[1:])
-            # 将路径拼接到服务器根目录
-            full_path = os.path.normpath(os.path.join(DIRECTORY, path))
-
-            # 如果路径在服务器根目录之外，则返回404 Not Found
-            if not full_path.startswith(os.path.normpath(DIRECTORY)):
-                self.send_error(404, "File not found")
-                return
-
-            # 如果是目录，则列出目录内容
-            if os.path.isdir(full_path):
-                self.list_directory(full_path)
-                return
-
-            # 如果是文件，则发送文件内容
-            if os.path.isfile(full_path):
-                with open(full_path, 'rb') as file:
-                    self.send_response(200)
-                    self.send_header('Content-type', cgi.guess_type(full_path)[0])
-                    self.end_headers()
-                    self.copyfile(file, self.wfile)
-                return
-        except Exception as e:
-            self.send_error(404, 'File not found: %s' % e)'''
-
 #创建服务器
 with socketserver.TCPServer(('', PORT), MyHandler) as httpd:
     print("serving at port", PORT)
