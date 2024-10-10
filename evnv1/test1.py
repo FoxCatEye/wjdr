88e969af10fff03c79dc91ef7b487850bcaeb6b4
猫耳 <1033578021@qq.com>
2024-10-10 14:20:21 +0800

    1.提交新版本窗口文件

---
+++ evnv1/test1.py
@@ -0,0 +1,32 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPalette, QBrush, QPixmap
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("背景图片示例")
+        self.set_background_image('11(1).png')  # 替换为您的图片路径
+
+    def set_background_image(self, image_path):
+        # 设置窗口的背景图片
+        palette = QPalette()
+        palette.setBrush(QPalette.Background, QBrush(QPixmap(image_path)))
+        self.setPalette(palette)
+
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPalette, QBrush, QPixmap
+
+class MyApp(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MyApp, self).__init__(parent)
+        self.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = MyApp()
+    mainWindow.show()
+    sys.exit(app.exec_())
