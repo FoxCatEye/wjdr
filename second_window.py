2470ac43c46ec5b1952ec299e131e00619911a44
猫耳 <1033578021@qq.com>
2025-02-08 17:52:23 +0800

    2.3.1； 1.优化：主页检查未识别到图案时，点击固定坐标位置的点位（废弃，第7点） 2.优化：仓库未检查到时未关闭左侧弹窗 3.优化：野怪，联盟宝箱，仓库执行时间优化（重复） 4.bug修复：体力概率未领取（体力检查在仓库补给流程内，长时间没补给导致未检查） 5.bug修复：循环时间等待中停止不生效(点击停止时，调用的是已废弃的单项停止组件，疑似程序崩溃也是该原因) 6.bug修复：隐藏UI崩溃问题（界面优化废弃两个区域，隐藏/显示函数内未注释） 7.优化主页检查逻辑（调用模拟器返回键）

--- second_window.py
+++ second_window.py
@@ -104,7 +104,7 @@ class Ui_NoticeWindow(object):
                                                          "<p style=\" margin-top:0px; margin-bottom:0px;\">    3.bug修复：隐藏UI崩溃问题</p>\n"
                                                          "<p style=\" margin-top:0px; margin-bottom:0px;\">    4.优化：野怪，联盟宝箱，仓库执行时间优化（重复）</p>\n"
                                                          "<p style=\" margin-top:0px; margin-bottom:0px;\">    5.优化：仓库未检查到时未关闭左侧弹窗</p>\n"
-                                                         "<p style=\" margin-top:0px; margin-bottom:0px;\">    6.优化：主页检查未识别到图案时，点击固定坐标位置的点位</p>\n"
+                                                         "<p style=\" margin-top:0px; margin-bottom:0px;\">    6.优化：主页检查逻辑</p>\n"
                                                          "<p style=\" margin-bottom:0px;\"></p>\n"
                                                          "<p style=\" margin-top:0px; margin-bottom:0px;\">    </p>\n"))
         self.textEdit.setReadOnly(True)
