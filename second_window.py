7d196a4d0b4e69c051c667a8caa41376c8fb58c9
猫耳 <1033578021@qq.com>
2025-06-29 14:32:08 +0800

    V2.3.8 1.优化采集资源检查 2.优化搜索逻辑 3.优化-模拟器地址/IP显示 4.优化-联盟宝箱，每日任务，生命之树，晨曦回礼定时逻辑 5.服务器逻辑

--- second_window.py
+++ second_window.py
@@ -157,6 +157,8 @@ class Ui_NoticeWindow(object):
                                                          "<p style=\" margin-top:0px; margin-bottom:0px;\">    1.优化-采集资源检查</p>\n"
                                                          "<p style=\" margin-top:0px; margin-bottom:0px;\">    2.优化-搜索野怪/矿物等逻辑</p>\n"
                                                          "<p style=\" margin-top:0px; margin-bottom:0px;\">    3.优化-模拟器地址/ip显示</p>\n"
+                                                         "<p style=\" margin-top:0px; margin-bottom:0px;\">    4.优化-联盟宝箱，每日任务，生命之树，晨曦回礼定时逻辑</p>\n"
+                                                         "<p style=\" margin-top:0px; margin-bottom:0px;\">    5.代码优化删除</p>\n"
                                                          "<p style=\" margin-bottom:0px;\"></p>\n"
                                                          "<p style=\" margin-top:0px; margin-bottom:0px;\">    </p>\n"))
         self.textEdit.setReadOnly(True)  # 设置文本不可编辑状态
